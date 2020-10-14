e81d385ce65e5dba7e4d8011ccc28219a5b2f31f
Edward Tirado Jr <ed@etdevelopment.net>
2020-10-14 16:16:50 -0600

    moved top 10 to its own intent file

--- __init__.py
+++ __init__.py
@@ -7,14 +7,18 @@ class Podcasts(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('podcasts.intent')
-    def handle_podcasts(self, message):
+    @intent_file_handler('top.intent')
+    def handle_top_podcasts(self, message):
         client = public.PublicClient()
         toplist = client.get_toplist()
 
         for index, entry in enumerate(toplist):
             self.speak_dialog(entry.title)
+            self.log.info(entry)
 
+    @intent_file_handler('podcasts.intent')
+    def handle_playback(self, message):
+        pass
 
 def create_skill():
     return Podcasts()
